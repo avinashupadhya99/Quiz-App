6003b771fc416effc66a7f048d9030b2613ca92a
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2020-03-29 12:19:10 +0530

    Added index, about and faq with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'about' => 'pages#about'
+
+  get 'faq' => 'pages#faq'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
 end
