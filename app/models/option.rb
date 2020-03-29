a66ceb2df96aa32aab03c2cef5635fea630108d5
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2020-03-29 21:33:46 +0530

    Added relations between models

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,3 +1,3 @@
 class Option < ApplicationRecord
-	has_and_belongs_to_many :questions
+	has_many :questions
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ApplicationRecord
-	has_and_belongs_to_many :options
-
+	has_many :quizzes
+	belongs_to :option
 end
