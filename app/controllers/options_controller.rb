33bbb787981046ee7ec0e5e3205cbd1e4246370e
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2020-04-04 22:36:59 +0530

    Cleaned code

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -15,7 +15,7 @@ class OptionsController < ApplicationController
       end
     end
     if @option.save
-      if @answer.present?
+      if @option.is_answer==true && @answer.present?
         @answer.is_answer=false
         @answer.save
       end
@@ -41,7 +41,7 @@ class OptionsController < ApplicationController
       end
     end
     if @each_option.update(option_params)
-      if @answer.present?
+      if @option.is_answer==true && @answer.present?
         @answer.is_answer=false
         @answer.save
       end
