aa215f4079afc2848d076c5b2e74f10fd3d72bc0
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2020-04-04 16:53:23 +0530

    Edit of options and styling of options forms

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -6,7 +6,19 @@ class OptionsController < ApplicationController
   def create
     @option = Option.new(option_params)
     @question = Question.find(params[:option][:question_id].to_i)
+    @option_all = Option.where(question_id: @question.id)
+    if @option_all.present?
+      @option_all.each do |each_option|
+        if each_option.is_answer==true
+          @answer=each_option
+        end
+      end
+    end
     if @option.save
+      if @answer.present?
+        @answer.is_answer=false
+        @answer.save
+      end
       flash[:success] = "Option was successfully created"
       redirect_to controller: 'quizzes', action: 'show', id: @question.quiz_id
     else
@@ -18,6 +30,26 @@ class OptionsController < ApplicationController
   end
 
   def update
+    @each_option = Option.find(params[:id])
+    @question = Question.find(@each_option.question_id)
+    @option_all = Option.where(question_id: @question.id)
+    if @option_all.present?
+      @option_all.each do |each_option|
+        if each_option.is_answer==true
+          @answer=each_option
+        end
+      end
+    end
+    if @each_option.update(option_params)
+      if @answer.present?
+        @answer.is_answer=false
+        @answer.save
+      end
+      flash[:success] = "Option was successfully updated"
+      redirect_to controller: 'quizzes', action: 'show', id: @question.quiz_id
+    else
+      redirect_to controller: 'quizzes', action: 'show', id: @question.quiz_id
+    end
   end
 
   def show
@@ -30,7 +62,7 @@ class OptionsController < ApplicationController
 
   def option_params
 
-    params.require(:option).permit(:opt_name, :question_id)
+    params.require(:option).permit(:opt_name, :is_answer, :question_id)
 
   end
 end
--- app/models/option.rb
+++ app/models/option.rb
@@ -1,3 +1,4 @@
 class Option < ApplicationRecord
 	belongs_to :question
+	validates :opt_name, presence: true, length: {minimum: 3, maximum: 20}
 end
