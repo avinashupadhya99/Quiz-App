fc10566ad88a02aa0ecc91c6ca8b9a59031bdf60
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2020-04-05 17:44:20 +0530

    Added uniqueness validation on options

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -7,22 +7,35 @@ class OptionsController < ApplicationController
     @option = Option.new(option_params)
     @question = Question.find(params[:option][:question_id].to_i)
     @option_all = Option.where(question_id: @question.id)
+    can_save=true
     if @option_all.present?
       @option_all.each do |each_option|
+        if each_option.opt_name==@option.opt_name
+          can_save=false
+        end
         if each_option.is_answer==true
           @answer=each_option
         end
       end
     end
-    if @option.save
-      if @option.is_answer==true && @answer.present?
-        @answer.is_answer=false
-        @answer.save
-      end
-      flash[:success] = "Option was successfully created"
+
+    if can_save==false
+      err_message = "Option "
+      err_message.concat(@option.opt_name)
+      err_message.concat(" already exists")
+      flash[:danger] = err_message
       redirect_to controller: 'quizzes', action: 'show', id: @question.quiz_id
     else
-      redirect_to controller: 'quizzes', action: 'show', id: @question.quiz_id
+      if @option.save
+        if @option.is_answer==true && @answer.present?
+          @answer.is_answer=false
+          @answer.save
+        end
+        flash[:success] = "Option was successfully created"
+        redirect_to controller: 'quizzes', action: 'show', id: @question.quiz_id
+      else
+        redirect_to controller: 'quizzes', action: 'show', id: @question.quiz_id
+      end
     end
   end
 
@@ -33,22 +46,35 @@ class OptionsController < ApplicationController
     @each_option = Option.find(params[:id])
     @question = Question.find(@each_option.question_id)
     @option_all = Option.where(question_id: @question.id)
+    can_save=true
     if @option_all.present?
-      @option_all.each do |each_option|
-        if each_option.is_answer==true
-          @answer=each_option
+      @option_all.each do |each_options|
+        if each_options.opt_name==@each_option.opt_name
+          can_save=false
+        end
+        if each_options.is_answer==true
+          @answer=each_options
         end
       end
     end
-    if @each_option.update(option_params)
-      if @each_option.is_answer==true && @answer.present?
-        @answer.is_answer=false
-        @answer.save
-      end
-      flash[:success] = "Option was successfully updated"
+
+    if can_save==false
+      err_message = "Option \""
+      err_message.concat(@each_option.opt_name)
+      err_message.concat("\" already exists")
+      flash[:danger] = err_message
       redirect_to controller: 'quizzes', action: 'show', id: @question.quiz_id
     else
-      redirect_to controller: 'quizzes', action: 'show', id: @question.quiz_id
+      if @each_option.update(option_params)
+        if @each_option.is_answer==true && @answer.present?
+          @answer.is_answer=false
+          @answer.save
+        end
+        flash[:success] = "Option was successfully updated"
+        redirect_to controller: 'quizzes', action: 'show', id: @question.quiz_id
+      else
+        redirect_to controller: 'quizzes', action: 'show', id: @question.quiz_id
+      end
     end
   end
 
