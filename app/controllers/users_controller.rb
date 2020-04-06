b79bf3863c6e3428aab60f7e3323cbe279626e41
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2020-04-06 21:54:19 +0530

    Added new user through UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,23 @@
 class UsersController < ApplicationController
 
 	def new
-
 		@user = User.new
-
 	end
 
 	def create
-
 		@user = User.new(user_params)
 		if @user.save
-			flash[:success] = "Welcome to the quiz app #{@user.username}"
-			redirect_to articles_path
+			flash[:success] = "Welcome to the quiz app, #{@user.username}"
+			redirect_to quizzes_path
 		else
 			render 'new'
 		end
-
 	end
 
 	private
 
 	def user_params
-
 		params.require(:user).permit(:username, :email, :password)
-
 	end
 
 end
