9d844ae2c14278dfae0f87866655f29f4ab27671
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2020-04-12 17:24:45 +0530

    Added star for top scorer

--- config/routes.rb
+++ config/routes.rb
@@ -33,8 +33,6 @@ Rails.application.routes.draw do
 
   post 'editPassword' => 'users#updatePassword'
 
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
